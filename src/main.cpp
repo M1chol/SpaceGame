5438ba1a1a50c40fc5b3dd7ed1a465ab0e8c79a0
Michał Kozłowski <micho.kozlowski@gmail.com>
2024-03-08 23:56:00 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,78 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "logger.h"
+
+using namespace std;
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* HelloWorld = NULL;
+
+bool loadMedia();
+bool init();
+void close();
+
+int main( int argc, char* args[] )
+{
+	LOG_INIT_CERR();
+	if (!init()){
+		log(LOG_ERR) << "Game failed during initialization\n"; 
+	}else{
+		if (!loadMedia()){
+			log(LOG_WARN) << "Media failed loading\n";
+		}else{
+			SDL_BlitSurface(HelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+			//Hack to get window to stay up
+   			SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+		}
+	}
+
+	close();
+	return 0;
+}
+
+bool init(){
+	LOG_INIT_CERR();
+	bool success = true;
+	if ( SDL_Init(SDL_INIT_VIDEO) < 0){
+		log(LOG_ERROR) << "SDL could not initialize successfuly " << SDL_GetError() << "\n";
+		success = false;
+	} else {
+		// Create window
+		gWindow = SDL_CreateWindow("Game",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if(gWindow == NULL){
+			log(LOG_ERR) << "Failed creating window " << SDL_GetError() << "\n";
+			success = false;
+		}
+		gScreenSurface = SDL_GetWindowSurface(gWindow);
+	}
+	return success;
+}
+
+bool loadMedia(){
+	LOG_INIT_CERR();
+	bool success = true;
+	HelloWorld = SDL_LoadBMP("res/player-placeholder.bmp");
+	if(HelloWorld == NULL){
+		log(LOG_WARN) << "Media could not be loaded " << SDL_GetError() << "\n";
+		success = false;
+	}
+	return success;
+}
+
+void close(){
+	// Dealocate surface
+	SDL_FreeSurface(HelloWorld);
+	HelloWorld = NULL;
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
+}
