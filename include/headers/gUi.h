2ae685ccd1b5e2dbc113b4b6ae576d73d2fcfe2a
Michal Kozlowski <m1chol@proton.me>
2025-03-25 16:51:40 +0100

    Can now render spheres

--- include/headers/gUi.h
+++ include/headers/gUi.h
@@ -1,5 +1,25 @@
 #include "engine.h"
 
+class uiRoundedRect : public Object
+{
+public:
+    uiRoundedRect(Scene *scene, int resolution, float radius);
+    void render() override;
+    void setText(std::string newValue) {textValue = newValue;};
+    void setResolution(int newResolution) {resolution = newResolution;};
+private:
+    std::string textValue;
+    SDL_Vertex getPosition(int index);
+    int *generateIndexes();
+    int resolution;
+    SDL_Vertex *vertices;
+    float radius;
+    SDL_Color color;
+    SDL_Color borderColor;
+    int *indexes;
+    int nrOfIndexes;
+};
+
 class MainMenu : public Scene
 {
 public:
--- src/gUi.cpp
+++ src/gUi.cpp
@@ -1,10 +1,78 @@
 #include "engine.h"
 
+#pragma region uiRoundedRect
+
+uiRoundedRect::uiRoundedRect(Scene *scene, int newRes, float radius) : Object(scene) 
+{
+    this->radius = radius;
+    if(newRes % 4 != 0){
+        log(LOG_WARN) << name << " - resolution is not a multiple of 4\n";
+        newRes = newRes - newRes % 4;
+        if(newRes < 4){
+            newRes = 4;
+        }
+    }
+    this->resolution = newRes;
+    color = {255, 255, 255, 255};
+    borderColor = {0, 0, 0, 255};
+    vertices = new SDL_Vertex[this->resolution];
+    nrOfIndexes = resolution + (resolution - 3) * 2;
+    indexes = generateIndexes();
+    for(int i = 0; i < nrOfIndexes; i++){
+        std::cout << indexes[i] << " ";
+    }
+    log(LOG_INFO) << "uiRoundedRect created\n";
+}
+
+void uiRoundedRect::render(){
+    Object::render();
+    for(int i=0; i < resolution; i++){
+        SDL_Vertex point = getPosition(i);
+        point.position.x += pos.x;
+        point.position.y += pos.y;
+        vertices[i] = point;
+    }
+    SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+    if(SDL_RenderGeometry(gRenderer, NULL, vertices, resolution, indexes, nrOfIndexes) < 0){
+        std::cout << "Error rendering geometry: " << SDL_GetError() << std::endl;
+    }
+
+    for(int i=0; i < resolution; i++){
+        SDL_RenderDrawPoint(gRenderer, vertices[i].position.x, vertices[i].position.y);
+    }
+
+}
+
+SDL_Vertex uiRoundedRect::getPosition(int index){
+    float angle = 2 * M_PI / resolution * index;
+    return {sin(angle) * radius, cos(angle) * radius, color};
+}
+
+int *uiRoundedRect::generateIndexes(){
+    int *indexes = new int[nrOfIndexes];
+    int j = 0;
+    indexes[j++] = 0;
+    indexes[j++] = 1;
+    indexes[j++] = 2;
+
+    for(int i=2; i < resolution - 1; i++){
+        indexes[j++] = 0;
+        indexes[j++] = i;
+        indexes[j++] = i+1;
+    }
+
+    return indexes;
+}
+
+
+#pragma endregion
+
 MainMenu::MainMenu()
 {
     Scene *uiScene = addScene("UI");
     uiScene->setDrawPriority(1);
     Object *title = new Object(uiScene);
+    title->setName("title");
     title->move({(double)SCREEN_WIDTH / 2, (double)SCREEN_HEIGHT / 4});
     TextComponent *text = new TextComponent("Scrap Wars", {0, 0}, fontSans, title);
     CustomUpdateComponent *update = new CustomUpdateComponent(title);
@@ -16,13 +84,13 @@ MainMenu::MainMenu()
         if (increasing)
         {
             angle += .1;
-            if (angle >= 15)
+            if (angle >= 10)
             increasing = false;
         }
         else
         {
             angle -= .1;
-            if (angle <= -15)
+            if (angle <= -10)
             increasing = true;
         }
 
@@ -31,15 +99,7 @@ MainMenu::MainMenu()
 
     text->setScale(2.0);
 
-    Object *startButton = new Object(uiScene);
-    startButton->move({(double)SCREEN_WIDTH / 2, title->getPos().y + 100});
-    TextComponent *startText = new TextComponent("Start", {0, 0}, fontSans, startButton);
-
-    Object *settingsButton = new Object(uiScene);
-    settingsButton->move({(double)SCREEN_WIDTH / 2, title->getPos().y + 200});
-    TextComponent *settingsText = new TextComponent("Settings", {0, 0}, fontSans, settingsButton);
-
-    Object *quitButton = new Object(uiScene);
-    quitButton->move({(double)SCREEN_WIDTH / 2, title->getPos().y + 300});
-    TextComponent *quitText = new TextComponent("Quit", {0, 0}, fontSans, quitButton);
+    uiRoundedRect *button = new uiRoundedRect(uiScene, 24, 100);
+    button->setName("button");
+    button->move({(double)SCREEN_WIDTH / 2, (double)SCREEN_HEIGHT / 2});
 };
